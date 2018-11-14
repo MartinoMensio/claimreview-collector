e9dff78d97ad71b15198a8407dab05d25626050d
Martino Mensio <martinomensio@outlook.it>
2018-11-14 17:23:53 +0000

    more sources and how to combine them

--- aggregate.py
+++ aggregate.py
@@ -4,17 +4,58 @@
 
 import glob
 import shutil
+from pathlib import Path
 
 import utils
 
-results = []
-for part in glob.glob(str(utils.data_location / '*/result.json')):
-    content = utils.read_json(part)
-    results.extend(content)
+# decide here what to aggregate
+choice = {
+    'datacommons_factcheck': {
+        'urls': True,
+        'domains': False
+    },
+    'datacommons_feeds': {
+        'urls': True,
+        'domains': False
+    },
+    'mrisdal_fakenews': {
+        'urls': False,
+        'domains': True
+    },
+    'golbeck_fakenews': {
+        'urls': True,
+        'domains': False
+    },
+    'liar': {
+        'urls': True,
+        'domains': False
+    },
+    'buzzface': {
+        'urls': True,
+        'domains': False
+    },
+    'opensources': {
+        'urls': False,
+        'domains': True
+    }
+}
 
-print(len(results), len(set([el['url'] for el in results])))
+all_urls = []
+all_domains = []
+for subfolder, config in choice.items():
+    if config['urls']:
+        urls = utils.read_json(utils.data_location / subfolder / 'urls.json')
+        all_urls.extend(urls)
+    if config['domains']:
+        domains = utils.read_json(utils.data_location / subfolder / 'domains.json')
+        all_domains.extend(domains)
 
-utils.write_json_with_path(results, utils.data_location, 'all.json')
+
+print(len(all_urls), len(all_domains))
+
+utils.write_json_with_path(all_urls, utils.data_location, 'all_urls.json')
+utils.write_json_with_path(all_domains, utils.data_location, 'all_domains.json')
 
 # copy to backend
-shutil.copy(utils.data_location / 'all.json', '../backend/urls.json')
+utils.write_json_with_path(all_urls, Path('../backend'), 'all_urls.json')
+utils.write_json_with_path(all_domains, Path('../backend'), 'all_domains.json')
--- opensources.py
+++ opensources.py
@@ -17,7 +17,16 @@ mappings = {
 
 properties = ['type', '2nd type', '3rd type']
 
-# TODO find the properties belonging to the mappings in the samples, and assign a single label
+results = []
+# find the properties belonging to the mappings in the samples, and assign a single label
 for domain, props in data.items():
     looking_at = [prop_value for prop_name, prop_value in props.items() if prop_name in properties and prop_value]
-    print(looking_at)
+    #print(looking_at)
+    classes = set(mappings[el] for el in looking_at if el in mappings)
+    if len(classes) != 1:
+        print(domain, classes)
+        continue
+    label = classes.pop()
+    results.append({'domain': domain, 'label': label, 'source': 'opensources'})
+
+utils.write_json_with_path(results, subfolder, 'domains.json')
