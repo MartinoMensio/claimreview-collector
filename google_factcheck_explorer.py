a3f7be0c1d75f9ff0e62779757afec4c6f2dce08
Martino Mensio <martino.mensio@open.ac.uk>
2019-01-29 11:30:49 +0000

    update scraping google toolbox

--- google_factcheck_explorer.py
+++ google_factcheck_explorer.py
@@ -1,11 +1,15 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import json
 import requests # not cache manager
 import os
+import plac
 
 from dotenv import load_dotenv, find_dotenv
 from collections import defaultdict
+
+from pprint import pprint
+from itertools import groupby
 
 import utils
 import claimreview
@@ -13,7 +17,7 @@ import claimreview
 load_dotenv(find_dotenv())
 subfolder_path = utils.data_location / 'google_factcheck_explorer'
 
-def get_recent(lang='', offset=0, num_results=200000, query='list:recent'):
+def get_recent(lang='', offset=0, num_results=1000, query='list:recent'):
     params = {
         'hl': lang, # the language to search
         'num_results': num_results,
@@ -39,11 +43,12 @@ def get_recent(lang='', offset=0, num_results=200000, query='list:recent'):
 
     content = json.loads(response.text[5:])
     reviews = content[0][1]
-    #with open('test.json', 'w') as f:
-    #    json.dump(content, f, indent=2)
+    utils.write_json_with_path(content, subfolder_path / 'intermediate', 'raw_{}.json'.format(offset))
 
     results = []
-    for r in reviews:
+    for idx, r in enumerate(reviews):
+        #print(idx)
+        #pprint(r)
         try:
             claimReview = {
                 '@context': "http://schema.org",
@@ -60,9 +65,9 @@ def get_recent(lang='', offset=0, num_results=200000, query='list:recent'):
                 },
                 'reviewRating': {
                     '@type': 'Rating',
-                    'ratingValue': r[0][3][0][9][0] if (len(r[0][3][0]) > 9 and  len(r[0][3][0][9])) else -1,
-                    'worstRating': r[0][3][0][9][1] if (len(r[0][3][0]) > 9 and  len(r[0][3][0][9])) else -1,
-                    'bestRating': r[0][3][0][9][2] if  (len(r[0][3][0]) > 9 and len(r[0][3][0][9])) else -1,
+                    'ratingValue': r[0][3][0][9][0] if (len(r[0][3][0]) > 9 and r[0][3][0][9] and len(r[0][3][0][9])) else -1,
+                    'worstRating': r[0][3][0][9][1] if (len(r[0][3][0]) > 9 and r[0][3][0][9] and len(r[0][3][0][9])) else -1,
+                    'bestRating': r[0][3][0][9][2] if  (len(r[0][3][0]) > 9 and r[0][3][0][9] and len(r[0][3][0][9])) else -1,
                     'alternateName': r[0][3][0][3],
                     #"image": ?,
                 },
@@ -84,41 +89,62 @@ def get_recent(lang='', offset=0, num_results=200000, query='list:recent'):
         except IndexError as e:
             print(json.dumps(r))
             raise(e)
-    print(len(results))
-    return results, content
+    #print(len(results))
+    return results
 
-claimReviews, raw = get_recent()
+def scrape():
+    offset = 0
+    claimReviews = []
+    while True:
+        print('offset', offset)
+        claims = get_recent(offset=offset)
+        if not claims:
+            break
+        offset += len(claims)
+        claimReviews.extend(claims)
 
-utils.write_json_with_path(claimReviews, subfolder_path, 'claimReviews.json')
-utils.write_json_with_path(raw, subfolder_path / 'intermediate', 'raw.json')
+    utils.write_json_with_path(claimReviews, subfolder_path, 'claimReviews.json')
+    return claimReviews
 
-urls = []
-rebuttals = defaultdict(lambda: defaultdict(list))
-for j, claimReview in enumerate(claimReviews):
-    claim_urls = claimreview.get_claim_urls(claimReview)
-    fixed_url = claimReview['url']
-    if claim_urls:
-        rebuttals[claim_urls][fixed_url] = ['google_factcheck_explorer']
-        score = claimreview.get_claim_rating(claimReview)
-        print(score)
-        label = None
-        if score != None:
-            # convert to fake/true
-            if score <= 0.30:
-                label = 'fake'
-            if score >= 0.8:
-                label = 'true'
-        if label:
-            urls.append({'url': claim_urls, 'label': label, 'source': 'google_factcheck_explorer'})
+def extract_urls_rebuttals_domains_factcheckers(claimReviews):
+    urls = []
+    rebuttals = defaultdict(lambda: defaultdict(list))
+    for j, claimReview in enumerate(claimReviews):
+        claim_urls = claimreview.get_claim_urls(claimReview)
+        fixed_url = claimReview['url']
+        if claim_urls:
+            rebuttals[claim_urls][fixed_url] = ['google_factcheck_explorer']
+            score = claimreview.get_claim_rating(claimReview)
+            print(score)
+            label = None
+            if score != None:
+                # convert to fake/true
+                if score <= 0.30:
+                    label = 'fake'
+                if score >= 0.8:
+                    label = 'true'
+            if label:
+                urls.append({'url': claim_urls, 'label': label, 'source': 'google_factcheck_explorer'})
 
-utils.write_json_with_path(claimReviews, subfolder_path, 'claimReviews.json')
-utils.write_json_with_path(rebuttals, subfolder_path, 'rebuttals.json')
+    utils.write_json_with_path(rebuttals, subfolder_path, 'rebuttals.json')
+    utils.write_json_with_path(urls, subfolder_path, 'urls.json')
+    by_domain = utils.compute_by_domain(urls)
+    utils.write_json_with_path(by_domain, subfolder_path, 'domains.json')
 
+    fact_checkers = list(set([utils.get_url_domain(el['url']) for el in claimReviews]))
+    lambda_aggregator = lambda el: utils.get_url_domain(el['url'])
+    fact_checkers = groupby(sorted(claimReviews, key=lambda_aggregator), key=lambda_aggregator)
+    fact_checkers = {k: len(list(v)) for k, v in fact_checkers}
+    utils.write_json_with_path(fact_checkers, subfolder_path, 'fact_checkers.json')
 
-utils.write_json_with_path(urls, subfolder_path, 'urls.json')
+def main(scraping=False):
+    print('scraping', scraping)
+    if scraping:
+        claimReviews = scrape()
+    else:
+        claimReviews = utils.read_json(subfolder_path / 'claimReviews.json')
 
-by_domain = utils.compute_by_domain(urls)
-utils.write_json_with_path(by_domain, subfolder_path, 'domains.json')
+    extract_urls_rebuttals_domains_factcheckers(claimReviews)
 
-fact_checkers = list(set([utils.get_url_domain(el['url']) for el in claimReviews]))
-utils.write_json_with_path(fact_checkers, subfolder_path, 'fact_checkers.json')
+if __name__ == "__main__":
+    plac.call(main)
