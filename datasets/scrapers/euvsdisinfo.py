b90fd400fac40c037a8077f9b326248ce5822ee6
Martino Mensio <martino.mensio@open.ac.uk>
2019-08-20 16:45:34 +0100

    euvsdisinfo and fullfact claimReview extraction

--- datasets/scrapers/euvsdisinfo.py
+++ datasets/scrapers/euvsdisinfo.py
@@ -4,18 +4,58 @@ import requests
 from bs4 import BeautifulSoup
 
 from ..processing import utils
-from ..processing import cache_manager
 
 LIST_URL = 'https://euvsdisinfo.eu/disinformation-cases/?offset={}'
 
-my_path = utils.data_location / 'euvsdisinfo'
+my_name = 'euvsdisinfo'
+my_path = utils.data_location / my_name
 
-def main():
+def get_credibility_measures(original_review):
+    return {'credibility': -1.0, 'confidence': 1.0}
+
+def create_claim_reviews(all_reviews):
+    claim_reviews = []
+
+    for review_url, review in all_reviews.items():
+        claim_urls = review['claim_urls'] + review['archived_claim_urls']
+
+        claim_review = {
+            "@context": "http://schema.org",
+            "@type": "ClaimReview",
+            "url": review_url,
+            "author": {
+                "@type": "Organization",
+                "name":"EU vs DISINFORMATION",
+                "url":"https://euvsdisinfo.eu/",
+                "sameAs": ["https://twitter.com/EUvsDisinfo", "https://www.facebook.com/EUvsDisinfo/"]
+            },
+            "claimReviewed": review['title'],
+            "reviewRating": {
+                "@type": "Rating",
+                "ratingValue": 1,
+                "bestRating": 5,
+                "worstRating": 1,
+                "alternateName": 'misinfo'
+            },
+            "itemReviewed": {
+                "@type": "Claim",
+                "appearance": [{'@type': 'CreativeWork', 'url': u} for u in claim_urls]
+            },
+            'origin': 'euvsdisinfo'
+        }
+
+        claim_reviews.append(claim_review)
+
+    utils.write_json_with_path(claim_reviews, my_path, 'claimReviews.json')
+    return claim_reviews
+
+
+def scrape():
     offset = 0
-    if os.path.exists(my_path / 'fact_checking_urls.json'):
-        all_statements = utils.read_json(my_path / 'fact_checking_urls.json')
+    if os.path.exists(my_path / 'source' / 'all_reviews.json'):
+        all_reviews = utils.read_json(my_path / 'source' / 'all_reviews.json')
     else:
-        all_statements = []
+        all_reviews = {}
     go_on = True
     while go_on:
         facts_url = LIST_URL.format(offset)
@@ -37,57 +77,71 @@ def main():
             date = s.select('div.disinfo-db-date')[0].text.strip()
             #outlets = s.select('data-column="Outlets"')[0].text.strip()
             country = s.select('div.cell-country')[0].text.strip()
-            #date = re.sub(r'Posted on (.+) ')
-
-            response_article = cache_manager.get(url)
-            soup = BeautifulSoup(response_article, 'lxml')
-
-            shortlink = soup.select_one('link[rel="shortlink"]')['href']
-
-            details = soup.select('div.report-meta-item')
-            for d in details:
-                if 'Reported in:' in d.text:
-                    issue_id = d.text.replace('Reported in:', '').replace('Issue', '').strip()
-                elif 'Language:' in d.text:
-                    language = d.text.replace('Language:', '').strip()
-                elif 'Keywords:' in d.text:
-                    keywords = d.text.replace('Keywords:', '').strip()
-
-            report_summary = soup.select_one('div.report-summary-text').text.strip()
-
-            claim_urls_all = soup.select('div.report-disinfo-link')
-
-            claim_urls = claim_urls_all[0].select('a')
-            claim_urls = [el['href'] for el in claim_urls]
-            if len(claim_urls_all) > 1:
-                archived_claim_urls = claim_urls_all[1].select('a')
-                archived_claim_urls = [el['href'] for el in archived_claim_urls]
+            if url in all_reviews:
+                pass
+                """
+                # already found, stop now
+                go_on = False
+                print('already in all_reviews', url)
+                break
+                """
             else:
-                archived_claim_urls = []
+                response = requests.get(url)
+                if response.status_code != 200:
+                    raise ValueError(response.status_code)
+                article = response.text
+                soup = BeautifulSoup(article, 'lxml')
 
-            disproof = soup.select_one('div.report-disproof-text').text.strip()
+                shortlink = soup.select_one('link[rel="shortlink"]')['href']
+
+                details = soup.select('div.report-meta-item')
+                for d in details:
+                    if 'Reported in:' in d.text:
+                        issue_id = d.text.replace('Reported in:', '').replace('Issue', '').strip()
+                    elif 'Language:' in d.text:
+                        language = d.text.replace('Language:', '').strip()
+                    elif 'Keywords:' in d.text:
+                        keywords = d.text.replace('Keywords:', '').strip()
+
+                report_summary = soup.select_one('div.report-summary-text').text.strip()
+
+                claim_urls_all = soup.select('div.report-disinfo-link')
+
+                claim_urls = claim_urls_all[0].select('a')
+                claim_urls = [el['href'] for el in claim_urls]
+                if len(claim_urls_all) > 1:
+                    archived_claim_urls = claim_urls_all[1].select('a')
+                    archived_claim_urls = [el['href'] for el in archived_claim_urls]
+                else:
+                    archived_claim_urls = []
+
+                disproof = soup.select_one('div.report-disproof-text').text.strip()
 
 
-            all_statements.append({
-                'url': url,
-                'title': title,
-                'date': date,
-                'country': country,
-                'shortlink': shortlink,
-                'issue_id': issue_id,
-                'language': language,
-                'keywords': keywords,
-                'summary': report_summary,
-                'claim_urls': claim_urls,
-                'archived_claim_urls': archived_claim_urls,
-                'disproof': disproof,
-                'source': 'euvsdisinfo'
-            })
+                all_reviews[url] = {
+                    'url': url,
+                    'title': title,
+                    'date': date,
+                    'country': country,
+                    'shortlink': shortlink,
+                    'issue_id': issue_id,
+                    'language': language,
+                    'keywords': keywords,
+                    'summary': report_summary,
+                    'claim_urls': claim_urls,
+                    'archived_claim_urls': archived_claim_urls,
+                    'disproof': disproof,
+                    'source': my_name
+                }
 
-        print(len(all_statements))
+        print(len(all_reviews))
         #print(all_statements)
         offset += 10
 
+    utils.write_json_with_path(all_reviews, my_path / 'source', 'all_reviews.json')
+    return all_reviews
 
 
-    utils.write_json_with_path(all_statements, my_path, 'fact_checking_urls.json')
+def main():
+    all_reviews = scrape()
+    create_claim_reviews(all_reviews)
--- datasets/scrapers/fullfact.py
+++ datasets/scrapers/fullfact.py
@@ -4,18 +4,84 @@ import requests
 import os
 from bs4 import BeautifulSoup
 import dateparser
+from tqdm import tqdm
 
 from ..processing import utils
+from ..processing import claimreview
 
 facts_url = 'https://fullfact.org/'
 
 my_path = utils.data_location / 'fullfact'
 
-def main():
-    if os.path.exists(my_path / 'fact_checking_urls.json'):
-        all_statements = utils.read_json(my_path / 'fact_checking_urls.json')
+
+def postlist_selector(soup):
+    result = []
+    articles = soup.select('div.postlist-item')
+    for a in articles:
+        title_el = a.select_one('h2.postlist-item-heading a')
+        if not title_el:
+            # the newsletter element
+            continue
+        url = f'https://fullfact.org{title_el["href"]}'
+        subtitle = a.select_one('p')
+        article = {
+            'title': title_el.text.strip(),
+            'url': url,
+            'subtitle': subtitle.text.strip(),
+            'source': 'fullfact'
+        }
+        result.append(article)
+    return result
+
+def feed_selector(soup):
+    result = []
+    if not soup:
+        return []
+    for s in soup.select('li'):
+        url = 'https://fullfact.org' +  s.select('a')[0]['href']
+        title = s.select('a')[0].text.strip()
+        date = s.select('small.date')[0].text.strip()
+        date = dateparser.parse(date).isoformat()
+
+        article = {
+            'url': url,
+            'title': title,
+            'date': date,
+            'source': 'fullfact'
+        }
+        result.append(article)
+    return result
+
+
+def scrape_fb_3rd_party():
+    """The page https://fullfact.org/online/ contains all the debunks published for the Facebook 3rd party fact-checking"""
+    page = 1
+    debunks = {}
+    while True:
+        listing_page_url = f'https://fullfact.org/online/?page={page}'
+        print(listing_page_url)
+        response = requests.get(listing_page_url)
+        if response.status_code != 200:
+            print(response.status_code)
+            break
+        page_content = response.text
+        soup = BeautifulSoup(page_content, 'lxml')
+        articles = postlist_selector(soup)
+        for a in articles:
+            url = a['url']
+            debunks[url] = a
+
+        page += 1
+
+    print('online 3rd party', len(debunks))
+    return debunks
+
+
+def scrape_latest():
+    if os.path.exists(my_path / 'source' / 'latest.json'):
+        all_statements = utils.read_json(my_path / 'source' / 'latest.json')
     else:
-        all_statements = []
+        all_statements = {}
 
     print(facts_url)
     response = requests.get(facts_url)
@@ -25,26 +91,76 @@ def main():
 
     soup = BeautifulSoup(response.text, 'lxml')
 
-    for s in soup.select('div.news-feed #mostRecent li'):
-        url = 'https://fullfact.org' +  s.select('a')[0]['href']
-        title = s.select('a')[0].text.strip()
-        date = s.select('small.date')[0].text.strip()
-        date = dateparser.parse(date).isoformat()
+    articles = feed_selector(soup.select_one('div.news-feed #mostRecent'))
+    for a in articles:
+        url = a['url']
+        all_statements[url] = a
 
-        found = next((item for item in all_statements if (item['url'] == url and item['date'] == date)), None)
-        if found:
-            print('found')
-            break
+    print('latest', len(all_statements))
+    utils.write_json_with_path(all_statements, my_path / 'source', 'latest.json')
 
-        all_statements.append({
-            'url': url,
-            'title': title,
-            'date': date,
-            'source': 'fullfact'
-        })
+    return all_statements
 
-    print(len(all_statements))
+def deep_scrape():
+    result = {}
+    # nested by category
+    homepage = requests.get('https://fullfact.org/')
+    if homepage.status_code != 200:
+        raise ValueError(homepage.status_code)
+    soup = BeautifulSoup(homepage.text, 'lxml')
+    categories = soup.select('ul.nav-bar-categories li')
+    for c in categories:
+        category_url = f'https://fullfact.org{c.select_one("a")["href"]}'
+        print(category_url)
+        category = requests.get(category_url)
+        if category.status_code != 200:
+            raise ValueError(category.status_code)
+        soup = BeautifulSoup(category.text, 'lxml')
+        subcategories = soup.select('ul.debates-list li')
+        for sc in subcategories:
+            subcategory_url = f'https://fullfact.org{sc.select_one("a")["href"]}'
+            print('   ', subcategory_url)
+            subcategory = requests.get(subcategory_url)
+            if subcategory.status_code != 200:
+                raise ValueError(subcategory.status_code)
+            soup = BeautifulSoup(subcategory.text, 'lxml')
+            articles = feed_selector(soup.select_one('div.stories-feed'))
+            print(len(articles))
+            for a in articles:
+                url = a['url']
+
+                result[url] = a
+
+    utils.write_json_with_path(result, my_path / 'source', 'deep_scrape.json')
+    return result
 
 
 
-    utils.write_json_with_path(all_statements, my_path, 'fact_checking_urls.json')
+def scrape_all():
+    online_3rd = scrape_fb_3rd_party()
+    latest = scrape_latest()
+    deep = deep_scrape()
+    all_debunks = {**online_3rd, **latest, **deep}
+    print('total articles', len(all_debunks))
+
+    utils.write_json_with_path(all_debunks, my_path / 'source', 'all_debunks.json')
+    return all_debunks
+
+def get_claimreviews():
+    debunks = utils.read_json(my_path / 'source' / 'all_debunks.json')
+
+    claim_reviews = []
+    for debunk in tqdm(debunks.values()):
+        url = debunk['url']
+        claim_review = claimreview.retrieve_claimreview(url)[1]
+        #print(type(claim_review), claim_review)
+        claim_reviews.extend(claim_review)
+    utils.write_json_with_path(claim_reviews, my_path, 'claimReviews.json')
+
+    print(len(claim_reviews))
+    return claim_reviews
+
+
+def main():
+    #scrape_all()
+    get_claimreviews()
