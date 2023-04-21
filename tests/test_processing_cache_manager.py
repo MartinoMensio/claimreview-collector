868f0057a361e42d88c9b26cef2e16a0687ab6bc
Martino Mensio <martino.mensio@open.ac.uk>
2023-04-21 14:11:48 +0200

    test processsing_cache_manager

--- tests/test_processing_cache_manager.py
+++ tests/test_processing_cache_manager.py
@@ -1,26 +1,30 @@
+from claimreview_scraper.processing import cache_manager
+
 url = "https://www.datacommons.org/factcheck/download#fcmt-data"
 bitly_url = "https://bit.ly/test-bitly-datacommons"
+large_url = (
+    "https://storage.googleapis.com/datacommons-feeds/factcheck/latest/data.json"
+)
 
 
 def test_get():
-    from claimreview_scraper.processing import cache_manager
-
     page = cache_manager.get(url)
     assert "DataCommons.org" in page
     assert "ClaimReview" in page
 
 
 def test_get_with_unshorten():
-    from claimreview_scraper.processing import cache_manager
-
     page = cache_manager.get(bitly_url, unshorten=True, force_refresh=True)
     assert "DataCommons.org" in page
     assert "ClaimReview" in page
 
 
 def test_verify_false():
-    from claimreview_scraper.processing import cache_manager
-
-    page = cache_manager.get(url, unshorten=False, force_refresh=True, verify=False)
+    page = cache_manager.get(url, unshorten=False, verify=False)
     assert "DataCommons.org" in page
     assert "ClaimReview" in page
+
+
+def test_large_file():
+    page = cache_manager.get(large_url)
+    assert "TOO LARGE" in page
